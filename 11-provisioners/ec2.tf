019f7ebf0b031be0d3da4b3b1d0cf093c509d907
Manjula123-hi <danthulurimanjula@gmail.com>
2025-06-12 16:17:22 +0530

    terraform

---
+++ 11-provisioners/ec2.tf
@@ -0,0 +1,44 @@
+resource "aws_instance" "roboshop" {
+  count                  = length(var.instances)
+  ami                    = var.ami_id
+  instance_type          = var.environment == "dev" ? "t3.micro" : "t3.small"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+
+  tags = merge(var.common_tags, {
+    Component = var.instances[count.index]
+    Name      = var.instances[count.index]
+  })
+
+    provisioner "local-exec" {
+    command = "echo ${self.private_ip} > inventory"
+    #on_failure = continue
+  }
+
+  #  provisioner "local-exec" {
+  #   command = " echo 'instance is destroyed"
+  #   when  = destroy
+  # }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = var.sg_name
+  description = var.sg_description
+
+  ingress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = "-1"
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = var.from_port
+    to_port          = var.to_port
+    protocol         = "-1"
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  #tags = merge(var.common_tags,var.sg_tags)
+  tags = var.sg_tags
+}
